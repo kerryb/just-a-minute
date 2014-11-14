392cd91bc1eb5ef3337001c599fc04ea3e8ff2b0
Kerry Buckley <kerryjbuckley@gmail.com>
2014-11-14 15:15:39 +0000

    Basic countdown timer

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var app = angular.module("justAMinute", []);
+
+app.controller("timerController", ["$scope", "$interval", function($scope, $interval) {
+  $scope.timer = {
+    remaining: 60
+  };
+
+  $scope.tick = function() {
+    $scope.timer.remaining -= 0.1;
+  };
+
+  $scope.start = function() {
+    $scope.timer.ticker = $interval(this.tick, 100);
+  };
+
+  $scope.stop = function() {
+    $interval.cancel($scope.timer.ticker);
+  };
+}]);
