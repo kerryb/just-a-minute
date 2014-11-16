65ba10b54bd9311b783b0a0a2b5d996084239df3
Kerry Buckley <kerryjbuckley@gmail.com>
2014-11-16 16:42:36 +0000

    Add whistle at end of minute

--- js/app.js
+++ js/app.js
@@ -1,32 +1,41 @@
-var app = angular.module("justAMinute", ["xeditable"]);
+var app = angular.module("justAMinute", ["xeditable", "ngAudio"]);
 
 app.run(function(editableOptions) {
   editableOptions.theme = 'bs3';
 });
 
-app.controller("timerController", ["$scope", "$interval", function($scope, $interval) {
-  $scope.timer = {
-    remaining: 60,
-    progressStyle: function() {
-      return { width: this.remaining / 0.6 + "%" };
-    }
-  };
+app.controller("timerController", ["$scope", "$interval", "ngAudio",
+  function($scope, $interval, ngAudio) {
+    $scope.whistle = ngAudio.load("media/whistle.wav");
+    $scope.timer = {
+      remaining: 60,
+      progressStyle: function() {
+        return { width: this.remaining / 0.6 + "%" };
+      }
+    };
 
-  $scope.tick = function() {
-    $scope.timer.remaining -= 0.1;
-    if ($scope.timer.remaining <= 0) {
+    $scope.tick = function() {
+      if ($scope.timer.remaining > 0.1) {
+        $scope.timer.remaining -= 0.1;
+      } else {
+        $scope.timeUp();
+      }
+    };
+
+    $scope.timeUp = function() {
       $scope.stop();
-    }
-  };
+      $scope.whistle.play();
+    };
 
-  $scope.start = function() {
-    $scope.timer.ticker = $interval(this.tick, 100);
-  };
+    $scope.start = function() {
+      $scope.timer.ticker = $interval(this.tick, 100);
+    };
 
-  $scope.stop = function() {
-    $interval.cancel($scope.timer.ticker);
-  };
-}]);
+    $scope.stop = function() {
+      $interval.cancel($scope.timer.ticker);
+    };
+  }
+]);
 
 app.directive("player",function() {
   return {
