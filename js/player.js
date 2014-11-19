e970319b22693182287ec361c0f8167944271495
Kerry Buckley <kerryjbuckley@gmail.com>
2014-11-19 20:20:41 +0000

    Indicate which player has buzzed

--- js/player.js
+++ js/player.js
@@ -1,20 +1,30 @@
+app.service("players", ["sounds", function(sounds) {
+  this.buzz = function(_this) {
+    return function(playerNumber) {
+      _this.buzzedPlayer = playerNumber;
+      sounds.buzz();
+    };
+  }(this);
+}]);
+
 app.directive("player", function() {
   return {
     scope: {
-      name: "@",
-      key: "@",
+      number: "="
     },
 
     templateUrl: "player.html",
 
-    controller: function($scope, $document) {
-      $scope.buzz = function() {
-        $scope.buzzed = true;
-      }
+    controller: function($scope, $document, players) {
+      $scope.name = "Player " + $scope.number;
+
+      $scope.class = function() {
+        return { buzzed: players.buzzedPlayer === $scope.number };
+      };
 
       $document.bind("keypress", function(event) {
-        if (event.which == $scope.key) {
-          $scope.buzz();
+        if (event.which == $scope.number + 48) {
+          players.buzz($scope.number);
         }
       });
     }
--- js/sounds.js
+++ js/sounds.js
@@ -1,5 +1,10 @@
 app.service("sounds", ["ngAudio", function(ngAudio) {
+  this._buzz= ngAudio.load("media/buzz.wav");
   this._whistle = ngAudio.load("media/whistle.wav");
+
+  this.buzz = function() {
+    this._buzz.play();
+  };
 
   this.whistle = function() {
     this._whistle.play();
