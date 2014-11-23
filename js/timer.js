0df5bcdbc59264dadca17260719814e6742e1de7
Kerry Buckley <kerryjbuckley@gmail.com>
2014-11-23 11:23:24 +0000

    Remove this hackery from timer service
    Move creation of interval to controller

--- js/timer.js
+++ js/timer.js
@@ -1,50 +1,46 @@
-app.service("timer", ["$interval", "sounds", function($interval, sounds) {
-  this.running = false;
-  this.remaining = 60;
+app.factory("timer", ["$interval", "sounds",
+  function($interval, sounds) {
+    return {
+      running: false,
+      remaining: 60,
 
-  this.tick = function(_this) {
-    return function() {
-      if (_this.remaining > 0) {
-        _this.remaining -= 0.1;
-      } else {
-        _this.timeUp();
+      tick: function(timer) {
+        if (timer.remaining > 0) {
+          timer.running = true;
+          timer.remaining -= 0.1;
+        } else {
+          timer.timeUp();
+        }
+      },
+
+      timeUp: function() {
+        this.stop();
+        sounds.whistle();
+      },
+
+      stop: function() {
+        this.running = false;
+        $interval.cancel(this.ticker);
       }
     };
-  }(this);
+  }
+]);
 
-  this.timeUp = function(_this) {
-    return function() {
-      _this.stop();
-      sounds.whistle();
-    };
-  }(this);
-
-  this.start = function(_this) {
-    return function() {
-      _this.running = true;
-      _this.ticker = $interval(_this.tick, 100);
-    };
-  }(this);
-
-  this.stop = function(_this) {
-    return function() {
-      _this.running = false;
-      $interval.cancel(_this.ticker);
-    };
-  }(this);
-}]);
-
-app.controller("timerController", ["$scope", "$document", "timer",
-  function($scope, $document, timer) {
+app.controller("timerController", ["$scope", "$document", "$interval", "timer",
+  function($scope, $document, $interval, timer) {
     $scope.timer = timer;
 
     $scope.progressStyle = function() {
       return { width: timer.remaining / 0.6 + "%" };
     };
 
+    $scope.startTimer = function() {
+      timer.ticker = $interval(function() { timer.tick(timer) }, 100);
+    };
+
     $document.bind("keypress", function(event) {
       if(event.which === 32) {
-        timer.start();
+        startTimer();
       }
     });
   }
